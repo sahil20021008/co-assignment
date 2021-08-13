e54aab4a7ae80089dc66b9b3b6dcd35c8075bf2d
madhavvyas03 <85939142+madhavvyas03@users.noreply.github.com>
2021-08-13 19:46:29 +0530

    We have completed the first question !!!

--- Simple-Assembler/run.py
+++ Simple-Assembler/run.py
@@ -1,9 +1,14 @@
 #NOTE:THIS CODE IS WRITTEN IN PYTHON 3 
 import math
+
 import re
+
 import sys
+
 global qw 
+
 qw = {}
+
 global typea
 
 global typeb
@@ -22,12 +27,19 @@ global flags
 
 global opcode
 
+global list_error
+
+global count_error
+
+count_error=[]
+
+list_error = []
+
 register_dict = {"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
 
 flags=[0,0,0,0]
 
 reg_values = [0,0,0,0,0,0,0,0,"000000000000"+str(flags[0])+str(flags[1])+str(flags[2])+str(flags[3])]
-#'mov':'00010',
 opcode = {'add':'00000','sub':'00001', 'mov':'00011', 'ld':'00100', 'st':'00101', 'mul':'00110', 
 'div':'00111', 'rs':'01000', 'ls':'01001', 'xor':'01010', 'or':'01011', 'and':'01100', 'not':'01101', 
 'cmp':'01110', 'jmp':'01111', 'jlt':'10000', 'jgt':'10001', 'je':'10010', 'hlt':'10011'}
@@ -44,14 +56,14 @@ typee = ["jmp","jlt","jgt","je"]
 
 def type_a(a):
     unused = "00"
-    re_x = args[1]
-    re_y = args[2]
-    re_z = args[3]
     if len(args) != 4:
         return -1
     for i in range(1, 4):
-        if args[i] not in register_dict:  # changed the dictionary
+        if args[i] not in register_dict:
             return -1
+    re_x = args[1]
+    re_y = args[2]
+    re_z = args[3]
     opcode_num = opcode[a]
     reg_list_temp = list(register_dict.keys())
     pos1 = reg_list_temp.index(re_x)
@@ -60,12 +72,10 @@ def type_a(a):
     reg_x = int(reg_values[pos1])
     reg_y = int(reg_values[pos2])
     reg_z = int(reg_values[pos3])
-    # print(2) i didnt understand print(2) is it for debugging or is it necessary?
     if a == "add":
-        if reg_y+reg_z>65535:#bin(reg_y)[0] == bin(reg_z)[0]:
-            #if bin(reg_y + reg_z)[0] != bin(reg_z)[0]:
+        if reg_y+reg_z>65535:
             flags[0] = 1
-        reg_values[pos1] = reg_y + reg_z  # overflow value is added
+        reg_values[pos1] = reg_y + reg_z  
     elif a == "sub":
         if bin(reg_z) > bin(reg_y):
             flags[0] = 1
@@ -73,9 +83,9 @@ def type_a(a):
         else:
             reg_values[pos1] = reg_y - reg_z
     elif a == "mul":
-        if reg_y*reg_z>65535:#reg_y * reg_z > sys.maxsize:
+        if reg_y*reg_z>65535:
             flags[0] = 1
-        reg_values[pos1] = reg_y * reg_z  # overflowed value
+        reg_values[pos1] = reg_y * reg_z 
     elif a == "xor":
         reg_values[pos1] = reg_y ^ reg_z
     elif a == "or":
@@ -87,60 +97,6 @@ def type_a(a):
     if reg_values[pos1]>65535:
         reg_values[pos1]-=65536
     return opcode_num + unused + register_dict[args[1]] + register_dict[args[2]] + register_dict[args[3]]
-
-
-# def type_a(a):
-#     unused = "00"
-#     re_x = args[1]
-#     re_y = args[2]
-#     re_z = args[3]
-#     if len(args)!=4 :
-#         return -1
-#     opcode_num = opcode[a]
-#     reg_list_temp = list(register_dict.keys())
-#     pos1 = reg_list_temp.index(re_x)
-#     pos2 = reg_list_temp.index(re_y)
-#     pos3 = reg_list_temp.index(re_z)
-#     reg_x = int(reg_values[pos1])
-#     reg_y = int(reg_values[pos2])
-#     reg_z = int(reg_values[pos3])
-#     print (2)
-#     if a == "add":
-
-#         if bin(reg_y)[0] == bin(reg_z)[0]:
-#             if  bin(reg_y+reg_z)[0] != bin(reg_z)[0]:
-#                 flags[0] = 1
-
-#         else:
-#             reg_values[pos1] = reg_y + reg_z
-    
-#     elif a == "sub":
-
-#         if bin(reg_z)>bin(reg_y):
-#             flags[0] = 1
-#             reg_values[pos1] = 0
-#         else:
-#             reg_values[pos1] = reg_y - reg_z
-    
-#     elif a == "mul":
-#         if reg_y*reg_z> sys.maxsize:
-#             flags[0]=1
-#         else :
-#             reg_values[pos1]=reg_y*reg_z
-    
-#     elif a == "xor":
-#         reg_values[pos1] = reg_y^reg_z
-    
-#     elif a == "or":
-#         reg_values[pos1] = reg_y|reg_z
-
-    
-#     elif a == "and":
-#         reg_values[pos1] = reg_y&reg_z
-#     else:
-#         return "error"
-    
-#     return opcode_num + unused + register_dict[args[1]] + register_dict[args[2]] + register_dict[args[3]] 
 
 def type_b(a):
     opcode_num = opcode[str(a)]
@@ -166,30 +122,6 @@ def type_b(a):
     g = temp[0:int(len(temp) - len(q))]
 
     return opcode_num + register_dict[args[1]] + g + q
-
-
-# def type_b(a):
-#     opcode_num = opcode[str(a)]
-#     temp="00000000"
-    
-#     reg_x = args[1]
-#     reg_list_temp = list(register_dict.keys())
-#     pos1 = reg_list_temp.index(reg_x)
-#     n=int(args[2][1:])
-#     q = bin(n).replace("0b", "")
-    
-#     if a == "mov":
-#         reg_values[pos1] = args[2][1:]
-#         opcode_num="00010"
-    
-#     elif a == "rs":
-#         reg_values[pos1] = reg_values[pos1]>>n
-    
-#     elif a == "ls":
-#         reg_values[pos1] = reg_values[pos1]<<n
-#     g=temp[0:int(len(temp)-len(q))]
-    
-#     return opcode_num + register_dict[args[1]] + g + q
 
 def type_c(a):
     unused = "00000"
@@ -255,21 +187,20 @@ def type_c(a):
     return opcode_num+unused+register_dict[args[1]]+register_dict[args[2]]
 
 def type_d(a,count):
-    n=count 
+    n=count
+    
     q = bin(n).replace("0b", "")
     reg_list_temp = list(register_dict.keys())
     temp="00000000"
     reg_x=args[1]
     pos1 = reg_list_temp.index(reg_x)
-    x=args[2]
-    #qw[x]= count 
+    x=args[2] 
     opcode_num = opcode[a]
 
     if a=="ld":
         if args[2] not in var_name :
             return -1
         else : 
-            #reg_values[pos1]=count
             reg_values[pos1]=qw[x]
     elif a=="st":
         if args[2] not in var_name :
@@ -283,8 +214,7 @@ def type_d(a,count):
         
 def type_e(a):
     opcode_num = opcode[a]
-    q=args[1]
-    print(q)
+    q=args[1]+":"
     type_e_dict_temp = list(type_e_dict.keys())
     temp="00000000"
     if a=="jmp":
@@ -292,7 +222,7 @@ def type_e(a):
             return [-1,-1]
         else :
             pos1 = type_e_dict_temp.index(q)
-            j=type_e_dict[pos1]
+            j=type_e_dict[q]
             x = bin(j).replace("0b", "")
             g=temp[0:int(len(temp)-len(x))]
             d=opcode_num+"000"+g+q
@@ -304,15 +234,20 @@ def type_e(a):
                 return [-1,-1]
             else :
                 pos1 = type_e_dict_temp.index(q)
-                j=type_e_dict[pos1]
+                j=type_e_dict[q]
                 x = bin(j).replace("0b", "")
                 g=temp[0:int(len(temp)-len(x))]
                 d=opcode_num+"000"+g+x
                 
         else : 
             j=-1
-            d=-1
+            j=type_e_dict[q]
+            x = bin(j).replace("0b", "")
+            g=temp[0:int(len(temp)-len(x))]
+            d=opcode_num+"000"+g+x
+            
 
+    
     elif a=="jgt":
 
         if flags[2]==1 :
@@ -320,13 +255,18 @@ def type_e(a):
                 return [-1,-1]
             else :
                 pos1 = type_e_dict_temp.index(q)
-                j=type_e_dict[pos1]
-                x = bin(j).replace("0b", "")
+                j=type_e_dict[q]-1
+                j1=type_e_dict[q]-1-len(var_name)
+                x = bin(j1).replace("0b", "")
                 g=temp[0:int(len(temp)-len(x))]
                 d=opcode_num+"000"+g+x     
-        else : 
+        else :
             j=-1
-            d=-1
+            j1=type_e_dict[q]-1-len(var_name)
+            x = bin(j1).replace("0b", "")
+            g=temp[0:int(len(temp)-len(x))]
+            d=opcode_num+"000"+g+x
+            
 
     elif a=="je":
 
@@ -335,13 +275,16 @@ def type_e(a):
                 return [-1,-1]
             else :
                 pos1 = type_e_dict_temp.index(q)
-                j=type_e_dict[pos1]
+                j=type_e_dict[q]
                 x = bin(j).replace("0b", "")
                 g=temp[0:int(len(temp)-len(x))]
                 d=opcode_num+"000"+g+x     
         else : 
             j=-1
-            d=-1
+            j=type_e_dict[q]
+            x = bin(j).replace("0b", "")
+            g=temp[0:int(len(temp)-len(x))]
+            d=opcode_num+"000"+g+x
     list_i0_memadd_i1_bin = [j,d]
     return list_i0_memadd_i1_bin    
 
@@ -367,12 +310,14 @@ def main():
             count=count+1
             for bv in args :
                 if bv.find(":")!=-1:
+                    if bv not in list_error :
+                        list_error.append(bv)
+                    else :
+                        count_error.append(count)
                     type_e_dict[str(bv)]=count
-
-
-    print(type_e_dict)                
+    a=list(set(count_error))
     k=0
-    while k < n :
+    while k < n-1 :
         args = code[k].split()
         count1=count1+1
         for j in args :
@@ -380,28 +325,31 @@ def main():
                 args.remove(j)
             else :
                 continue
-        x=starter(args,count)
-        if x!=0 :
-            k=x
-        else :
+        if k in count_error :
+            code_output.append("ERROR\n")
             k=k+1
+        else :
+            x=starter(args,count1)
+            if x!=0 :
+                k=x
+            else :
+                k=k+1
     
     file1 = open('output.txt', 'w')
     file1.writelines(code_output)
     file1.close()
             
-def starter(arg,count):
-    #print(arg)
-    print(reg_values)
+def starter(arg,count1):
     global args
     args=arg
+    if len(args)==0 :
+        code_output.append("ERROR\n")
+        return 0
     if args[0] in typea   :
-        #print(1)
         code_out = type_a(args[0])
         if code_out==-1 :
             code_output.append("ERROR\n")
         else :
-            print(code_out)
             code_output.append(code_out+"\n")
         return 0
     elif args[0] in typeb and args[2][0:1]=="$" :
@@ -415,8 +363,7 @@ def starter(arg,count):
         return 0
 
     elif args[0] in typed :
-        count=count+1
-        code_out = type_d(args[0],count)
+        code_out = type_d(args[0],count1)
         if code_out==-1 :
             code_output.append("ERROR\n")
         else :
@@ -425,11 +372,12 @@ def starter(arg,count):
             
 
     elif args[0] in typee :
+        print(args[0])
         code_out = type_e(args[0])
         if code_out[0]==-1 :
-            code_output.append("ERROR\n")
+            code_output.append(str(code_out[1])+"asf\n")
             return 0
-        elif code_out[0]<=length :
+        elif code_out[0]<=n-1 :
             code_output.append(code_out[1]+"\n")
             i= code_out[0]
             return code_out[0]
@@ -449,13 +397,4 @@ def starter(arg,count):
         code_output.append("ERROR\n")
         return 0
 main()            
-            
-            
-        
-
-            
-            
-        
-        
-    
-
+     
