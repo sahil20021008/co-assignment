32834736126dea277f9f33a58855f55903200532
madhavvyas03 <85939142+madhavvyas03@users.noreply.github.com>
2021-08-24 11:03:04 +0530

    Update simulator.py

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -4,8 +4,6 @@ from sys import stdin
 
 import math
 
-import matplotlib as plt
-
 import re
 
 import sys
@@ -42,9 +40,7 @@ list_error = []
 
 register_dict = {"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
 
-flags=[0,0,0,0]
-
-reg_values = [0,0,0,0,0,0,0,"000000000000"+str(flags[0])+str(flags[1])+str(flags[2])+str(flags[3])]
+reg_values = [0,0,0,0,0,0,0,0]
 opcode = {'add':'00000','sub':'00001', 'mov':'00011', 'ld':'00100', 'st':'00101', 'mul':'00110', 
 'div':'00111', 'rs':'01000', 'ls':'01001', 'xor':'01010', 'or':'01011', 'and':'01100', 'not':'01101', 
 'cmp':'01110', 'jmp':'01111', 'jlt':'10000', 'jgt':'10001', 'je':'10010', 'hlt':'10011'}
@@ -58,6 +54,7 @@ typec = ["00011","00111","01110","01101"]
 typed = ["00100","00101"]
 
 typee = ["01111","10000","10001","10010"]
+flags=[0,0,0,0]
 
 def flagger(a=0, b=0, c=0, d=0):
     flags[0] = a
@@ -65,6 +62,7 @@ def flagger(a=0, b=0, c=0, d=0):
     flags[2] = c
     flags[3] = d
     reg_values[7] = "000000000000" + str(flags[0]) + str(flags[1]) + str(flags[2]) + str(flags[3])
+
 
 def type_a(a,arg,pc):
     
@@ -115,7 +113,7 @@ def type_a(a,arg,pc):
         return "error"
     while reg_values[pos1] > 65535:
         reg_values[pos1] -= 65536
-    return str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + f'{reg_values[7]:016b}'
+    return str(str(f'{count1:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + reg_values[7])
 
 # def type_a(a,count):
 #     unused = "00"
@@ -187,39 +185,9 @@ def type_b(a,arg,pc):
             flagger(1)
     while reg_values[pos1] > 65535:  # added overflow condition
         reg_values[pos1] -= 65536
-    return str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + f'{reg_values[7]:016b}'
+    return str(str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + reg_values[7])
 
 
-# def type_b(a,count1):
-#     opcode_num = opcode[str(a)]
-#     temp = "00000000"
-
-#     reg_x = args[1]
-#     reg_list_temp = list(register_dict.keys())
-#     pos1 = reg_list_temp.index(reg_x)
-#     #n = int(args[2][1:])
-#     boo=args[2][1:].isdigit()
-#     if not boo:
-#         return "Type error on line:" + str(count1) + " , int expected"
-#     n = int(args[2][1:])#moved n after checking if int
-#     if n<0 or n>255:
-#         return "Error on line :" + str(count1) + " , illegal immediate value\n"
-#     q = bin(n).replace("0b", "")
-#     flagger()
-#     if a == "mov":
-#         reg_values[pos1] = n
-#         opcode_num = "00010"
-#     elif a == "rs":
-#         reg_values[pos1] = reg_values[pos1] >> n
-#     elif a == "ls":
-#         reg_values[pos1] = reg_values[pos1] << n
-#         if reg_values[pos1] > 65535:#added overflow check
-#             flagger(1)
-#     while reg_values[pos1] > 65535: #added overflow condition
-#         reg_values[pos1] -= 65536
-#     g = temp[0:int(len(temp) - len(q))]
-
-#     return opcode_num + register_dict[args[1]] + g + q
 
 def type_c(a,arg,pc):
     unused = "00000"
@@ -236,8 +204,11 @@ def type_c(a,arg,pc):
 #    if len(args)!=3:
 #        return "Syntax error on line :" + str(pc) + " ,instructions of type C should have 3 arguments\n"
         
-    if a=="00011" :#turned if to elif
-        reg_values[pos1]=reg_values[pos2]
+    if a=="00011" :
+        if reg_y == "111" :
+            reg_values[pos1] = int(reg_values[7],2)
+        else:
+            reg_values[pos1]=reg_values[pos2]
         flagger()
         
     elif a=="00111" :
@@ -285,8 +256,9 @@ def type_c(a,arg,pc):
     
     else : 
     
-        return "Error"  
-    return str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + f'{reg_values[7]:016b}'
+        return "Error"
+    print(reg_values)
+    return str(str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + reg_values[7])
 
 def type_d(a,arg,pc):
 
@@ -305,7 +277,7 @@ def type_d(a,arg,pc):
         
     flagger()
     g=temp[0:int(len(temp)-len(q))]
-    return str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + f'{reg_values[7]:016b}'
+    return str(str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + reg_values[7])
 
 
 def type_e(a,arg,pc):
@@ -337,19 +309,17 @@ def type_e(a,arg,pc):
         if flags[3]==1 :
             q=q
         else : 
-            q=-1 
-    d=str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + f'{reg_values[7]:016b}'       
-    list_i0_memadd_i1_bin = [q,d]
+            q=-1
     flagger()
+    d=str(str(f'{pc:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + reg_values[7])
+    list_i0_memadd_i1_bin = [q,d]
+    
     
     return list_i0_memadd_i1_bin 
 
 def main():
-    code=[]
-    for line in stdin :
-        if line=="":
-            break
-        code.append(line)
+    file = open('code.txt', 'r')
+    code = file.readlines()
     global n
     n = len(code) + 1
     length = len(code)
@@ -377,28 +347,17 @@ def main():
     a=list(set(count_error))
     k=0
     while k < n-1 :
-        args = code[k].split()
+        args = code[k]
+        x=starter(args,count1)
         count1=count1+1
-        for j in args :
-            if j.find(":")!=-1 :
-                args.remove(j)
-            else :
-                continue
-        if k in count_error :
-            code_output.append("ERROR on line :" + str(count1) + "\n")
-            k=k+1
-        else :
-            x=starter(args,count1)
-            if x!=0 :
-                k=x
-            else :
-                k=k+1
+        k += 1
+        
     
-    
-    var=sys.stdout
-    for i in code_output:
-        var.write(i)
-         
+    file1 = open('output.txt', 'w')
+    file1.writelines(code_output)
+    file1.writelines(code)
+    file1.close()
+            
 def starter(arg,count1):
     global args
     type_checker = arg[slice(5)]
@@ -425,34 +384,26 @@ def starter(arg,count1):
         code_out = type_d(args,arg,count1)
         code_output.append(code_out+"\n")
         return 0
-            
-
-    elif args[0] in typee :
-        code_out = type_e(args[0],count1)
-        if code_out[1] == -1 :
-            code_out.append("Undefined label:" + str(count1) +"\n")
-            return 0
+    
+    elif args in typee :
+        
+        code_out = type_e(args,arg,count1)
         if code_out[0] == -1 :
             code_output.append(str(code_out[1])+"\n")
             return 0
-
-        elif code_out == -2:
-            code_output.append("Syntax error on line :" + str(count1) + " ,instructions of type E should have 2 arguments\n")
-
 
         elif code_out[0]<=n-1 :
             code_output.append(code_out[1]+"\n")
             i= code_out[0]
             return code_out[0]
 
-        else :
-            code_output.append("ERROR on line :" + str(count1) + "\n")
-            return 0
-        
+  
     elif args == "10011":
-        code_output.append(str(f'{count1:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + f'{reg_values[7]:016b}' + "\n")
+        code_output.append(str(str(f'{count1:08b}') + " " + f'{reg_values[0]:016b}' + " " + f'{reg_values[1]:016b}' + " " + f'{reg_values[2]:016b}' + " " + f'{reg_values[3]:016b}' + " " + f'{reg_values[4]:016b}' + " " + f'{reg_values[5]:016b}' + " " + f'{reg_values[6]:016b}' + " " + reg_values[7]) + "\n")
         return n
+    
     else:
-        code_output.append("ERROR on line :" + str(count1) + "\n")
+        code_output.append("madhav on line :" + str(count1) + "\n")
         return 0
-main()          
+main()
+        
